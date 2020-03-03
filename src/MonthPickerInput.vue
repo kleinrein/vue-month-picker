5cd24599e1369e79b88f7b7ca53b5883dd5a6038
Thiago Victorino <thiagovictorino@gmail.com>
2020-03-03 08:34:45 -0300

    Fix indentention project standards

--- src/MonthPickerInput.vue
+++ src/MonthPickerInput.vue
@@ -1,83 +1,82 @@
 <template>
   <div
-          class="month-picker-input-container"
-          v-click-outside="hide"
+      class="month-picker-input-container"
+      v-click-outside="hide"
   >
     <input
-            class="month-picker-input"
-            type="text"
-            v-model="selectedDate"
-            @click="showMonthPicker()"
-            readonly
+        class="month-picker-input"
+        type="text"
+        v-model="selectedDate"
+        @click="showMonthPicker()"
+        readonly
     >
     <month-picker
-            v-show="monthPickerVisible"
-            @input="populateInput"
-            :default-year="defaultYear"
-            :default-month="defaultMonth"
-            :lang="lang"
-            :months="months"
-            :no-default="noDefault"
-            :show-year="showYear"
-            :clearable="clearable"
-            :variant="variant"
-            :editable-year="editableYear"
+        v-show="monthPickerVisible"
+        @input="populateInput"
+        :default-year="defaultYear"
+        :default-month="defaultMonth"
+        :lang="lang"
+        :months="months"
+        :no-default="noDefault"
+        :show-year="showYear"
+        :clearable="clearable"
+        :variant="variant"
+        :editable-year="editableYear"
     >
     </month-picker>
   </div>
 </template>
 
 <script>
-  import Vue from 'vue'
-  import MonthPicker from './MonthPicker.vue'
-  import monthPicker from './month-picker'
+import MonthPicker from './MonthPicker.vue'
+import monthPicker from './month-picker'
 
-  export default {
-    name: 'en',
-    mixins: [monthPicker],
-    directives: {
-      clickOutside: {
-        bind: function (el, binding, vnode) {
-          el.event = function (event) {
-            if (!(el === event.target || el.contains(event.target))) {
-              vnode.context[binding.expression](event)
-            }
+export default {
+  name: 'en',
+  mixins: [monthPicker],
+  directives: {
+    clickOutside: {
+      bind: function (el, binding, vnode) {
+        el.event = function (event) {
+          if (!(el === event.target || el.contains(event.target))) {
+            vnode.context[binding.expression](event)
           }
-          document.body.addEventListener('click', el.event)
-        },
-        unbind: function (el) {
-          document.body.removeEventListener('click', el.event)
         }
-      }
-    },
-    data() {
-      return {
-        monthPickerVisible: false,
-        selectedDate: null
-      }
-    },
-    components: {
-      MonthPicker
-    },
-    mounted() {
-        if(this.inputPreFilled && this.defaultMonth !== null && this.defaultYear !== null){
-          this.selectedDate = `${this.monthsByLang[this.defaultMonth]}, ${this.defaultYear}`
-        }
-    },
-    methods: {
-      populateInput(date) {
-        this.selectedDate = `${date.month}, ${date.year}`
-        this.monthPickerVisible = false
-        this.$emit("input", date)
+        document.body.addEventListener('click', el.event)
       },
-      showMonthPicker() {
-        this.monthPickerVisible = !this.monthPickerVisible
-      },
-      hide() {
-        this.monthPickerVisible = false
+      unbind: function (el) {
+        document.body.removeEventListener('click', el.event)
       }
     }
+  },
+  data() {
+    return {
+      monthPickerVisible: false,
+      selectedDate: null
+    }
+  },
+  components: {
+    MonthPicker
+  },
+  mounted() {
+    if (this.inputPreFilled && this.defaultMonth !== null && this.defaultYear !== null) {
+      this.selectedDate = `${this.monthsByLang[this.defaultMonth]}, ${this.defaultYear}`
+    }
+  },
+  methods: {
+    populateInput(date) {
+      this.selectedDate = `${date.month}, ${date.year}`
+      this.monthPickerVisible = false
+      this.$emit("input", date)
+    },
+    showMonthPicker() {
+      this.monthPickerVisible = !this.monthPickerVisible
+    },
+    hide() {
+      this.monthPickerVisible = false
+    }
   }
+}
 </script>
 <style scoped>
   .month-picker-input-container {
